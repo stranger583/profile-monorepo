92b0b6a10ee4b99adde6d7ab2880a7aefa06d4ed
stranger583 <zed042760@gmail.com>
2024-07-27 21:51:35 +0800

    style:add button style

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`dark`}>
         {children}
       </body>
     </html>
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -5,16 +5,32 @@ import { cN } from "./utils/utils";
 export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   children: ReactNode;
   className?: string;
+  variant?: 'default' | 'outline'
+  color?: 'primary' | 'secondary' | 'light' | 'dark' | 'default'
 }
 
-const variant = {
-  
+const baseStyle = {
+  default: 'ui-rounded',
+  outline: 'ui-border ui-rounded-full',
 }
 
-export const Button = ({ children,className }: ButtonProps) => {
+const baseColor = {
+  default: '',
+  primary: 'ui-bg-primary ui-text-white',
+  secondary: 'ui-bg-secondary ui-text-white',
+  dark: 'ui-bg-black ui-text-white',
+  light: 'ui-bg-white ui-text-black',
+}
+
+export const Button = ({ variant = 'outline', color = 'default', children, className, ...props }: ButtonProps) => {
   return (
     <button
-      className= {cN(``,className)}
+      {...props}
+      className={
+        cN('ui-flex ui-items-center ui-justify-between ui-py-1 ui-px-3 disabled:ui-pointer-events-none',
+          baseStyle[variant], baseColor[color],
+          className
+        )}
     >
       {children}
     </button>
