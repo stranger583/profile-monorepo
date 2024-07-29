2765a61ec5862f7d6e6ff3b8ea055354a13a79ec
stranger583 <zed042760@gmail.com>
2024-07-29 23:39:09 +0800

    test: testing svg use methods

--- apps/web/app/[lang]/page.tsx
+++ apps/web/app/[lang]/page.tsx
@@ -5,122 +5,159 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Card, CardHeader, CardContent } from "@repo/ui/card";
-import { Section } from "@repo/ui/section"
+import { Section } from "@repo/ui/section";
 import { Button } from "@repo/ui/button";
 import { Input } from "@repo/ui/input";
 
-import me from '@public/images/me.jpg'
-import fish from '@public/images/fish.jpg'
-import bar from '@public/images/bar.jpg'
-import fuji from "@public/images/fuji.jpg"
-import lionTravel from '@public/images/lion-travel.jpg'
+import me from "@public/images/me.jpg";
+import fish from "@public/images/fish.jpg";
+import bar from "@public/images/bar.jpg";
+import fuji from "@public/images/fuji.jpg";
+import lionTravel from "@public/images/lion-travel.jpg";
 
 import ThemeButton from "@components/theme-button";
-import linkedin from '@public/svg/linkedin.svg'
-import github from '@public/svg/github.svg'
-import len from '@public/svg/len.svg'
-import arrow from '@public/svg/arrow.svg'
-import arrowButton from '@public/svg/arrow-button.svg'
-import reactSvg from "@public/skills/react.svg"
+import linkedin from "@public/svg/linkedin.svg";
+import github from "@public/svg/github.svg";
+import arrow from "@public/svg/arrow.svg";
+import arrowButton from "@public/svg/arrow-button.svg";
+import reactSvg from "@public/skills/react.svg";
 
 import MainSectionBlock from "@components/main-section-block";
 import ArticleBlock from "@components/article-card";
 
-import { Locale } from '../../i18n.config'
-import { getDictionary } from '../../get-dictionary'
+import { Locale } from "../../i18n.config";
+import { getDictionary } from "../../get-dictionary";
+import LangButton from "@components/lang-button";
 
 const mockArticleData = [
   {
-    imgUrl: './images/lion-travel.jpg',
-    link: './',
-    date: 'June 3, 2024',
-    skills: ['Nextjs', 'React', 'TailwindCSS'],
-    title: 'XinMedia',
-    description: 'i voluptates adipisci alias a nemo vel ipsamlit. Facere doloremque error debitis veniam similique. Corporis vitae',
+    imgUrl: "./images/lion-travel.jpg",
+    link: "./",
+    date: "June 3, 2024",
+    skills: ["Nextjs", "React", "TailwindCSS"],
+    title: "XinMedia",
+    description:
+      "i voluptates adipisci alias a nemo vel ipsamlit. Facere doloremque error debitis veniam similique. Corporis vitae",
   },
   {
-    imgUrl: './images/lion-travel.jpg',
-    link: './',
-    date: 'June 3, 2024',
-    skills: ['Nextjs', 'React', 'TailwindCSS'],
-    title: 'XinFlight',
-    description: 'i voluptates adipisci alias a nemo vel ipsamlit. Facere doloremque error debitis veniam similique. Corporis vitae',
+    imgUrl: "./images/lion-travel.jpg",
+    link: "./",
+    date: "June 3, 2024",
+    skills: ["Nextjs", "React", "TailwindCSS"],
+    title: "XinFlight",
+    description:
+      "i voluptates adipisci alias a nemo vel ipsamlit. Facere doloremque error debitis veniam similique. Corporis vitae",
   },
-]
-
+];
 
 export default async function Home({
-  params: { lang }
-}:{
-  params: { lang: Locale }
+  params: { lang },
+}: {
+  params: { lang: Locale };
 }) {
-  const { HomePage } = await getDictionary(lang)
+  const { HomePage } = await getDictionary(lang);
   return (
     <main className="flex gap-3 scrollbar-thin scrollbar-webkit">
-      <Section className="overflow-y-auto h-[calc(100dvh_-_32px)] w-[360px] flex flex-col gap-2 grow scrollbar-webkit">
+      <Section className="overflow-y-auto h-[calc(100dvh_-_32px)] w-[360px] flex flex-col gap-2 grow scrollbar-webkit border border-cardBorder">
         <Card className="relative overflow-hidden">
-          <Image src={fuji} alt="fuji" className="absolute z-10 left-0 -top-[110px] opacity-75" />
+          <Image
+            src={fuji}
+            alt="fuji"
+            className="absolute z-10 left-0 -top-[110px] opacity-75"
+          />
           <CardContent className="flex flex-col gap-2 relative z-20">
-            <Link href='' className="text-bold text-base">Home</Link>
-            <Link href='' className="text-bold text-base">About</Link>
-            <Link href='' className="text-bold text-base">Project</Link>
-            <Link href='' className="text-bold text-base">Article</Link>
+            <Link href="" className="text-bold text-base">
+              Home
+            </Link>
+            <Link href="" className="text-bold text-base">
+              About
+            </Link>
+            <Link href="" className="text-bold text-base">
+              Project
+            </Link>
+            <Link href="" className="text-bold text-base">
+              Article
+            </Link>
           </CardContent>
         </Card>
         <Card className="">
           <CardHeader>Tools </CardHeader>
           <CardContent className="flex gap-2">
-            <ThemeButton/>
-            <Button variant="default" className="p-0">
-              <Image src={len} alt="len" />
-            </Button>
+            <ThemeButton />
+            <LangButton />
           </CardContent>
         </Card>
         <Card>
           <CardHeader className="flex gap-2 items-center">
             Projects
-            <Image src={arrow} alt='arrow' />
+            <Image src={arrow} alt="arrow" />
           </CardHeader>
           <CardContent className="mb-4">
-            <Link href='' className="text-secondaryText text-sm">i voluptates adipisci alias a nemo vel</Link>
-            <Link href='' className="text-secondaryText text-sm">i voluptates adipisci alias a nemo vel</Link>
-            <Link href='' className="text-secondaryText text-sm">i voluptates adipisci alias a nemo vel</Link>
+            <Link href="" className="text-secondaryText text-sm">
+              i voluptates adipisci alias a nemo vel
+            </Link>
+            <Link href="" className="text-secondaryText text-sm">
+              i voluptates adipisci alias a nemo vel
+            </Link>
+            <Link href="" className="text-secondaryText text-sm">
+              i voluptates adipisci alias a nemo vel
+            </Link>
           </CardContent>
           <CardHeader className="flex gap-2 items-center">
             Articles
-            <Image src={arrow} alt='arrow' />
+            <Image src={arrow} alt="arrow" />
           </CardHeader>
           <CardContent>
-            <Link href='' className="text-secondaryText text-sm">i voluptates adipisci alias a nemo vel</Link>
-            <Link href='' className="text-secondaryText text-sm">i voluptates adipisci alias a nemo vel</Link>
-            <Link href='' className="text-secondaryText text-sm">i voluptates adipisci alias a nemo vel</Link>
+            <Link href="" className="text-secondaryText text-sm">
+              i voluptates adipisci alias a nemo vel
+            </Link>
+            <Link href="" className="text-secondaryText text-sm">
+              i voluptates adipisci alias a nemo vel
+            </Link>
+            <Link href="" className="text-secondaryText text-sm">
+              i voluptates adipisci alias a nemo vel
+            </Link>
           </CardContent>
         </Card>
         <Card className="p-0 overflow-hidden h-32 relative">
-          <Image src={fish} className="absolute -top-1/3 left-1/2 -translate-x-1/2" alt="fish" />
+          <Image
+            src={fish}
+            className="absolute -top-1/3 left-1/2 -translate-x-1/2"
+            alt="fish"
+          />
         </Card>
         <Card>
           <CardHeader>Links </CardHeader>
           <CardContent className="flex gap-2">
-            <Link href='https://www.linkedin.com/in/cozy-chen-508a68280/' target="_blank">
-            <Image src={linkedin} alt="dark mode"/>
+            <Link
+              href="https://www.linkedin.com/in/cozy-chen-508a68280/"
+              target="_blank"
+            >
+              <Image src={linkedin} alt="dark mode" />
             </Link>
-            <Link href='https://github.com/stranger583' target="_blank">
-              <Image src={github} alt="len"/>
+            <Link href="https://github.com/stranger583" target="_blank">
+              <Image src={github} alt="len" />
             </Link>
           </CardContent>
         </Card>
         <Card>
-          <CardHeader>Say something...  </CardHeader>
+          <CardHeader>Say something... </CardHeader>
           <CardContent className="flex items-center justify-between border border-input text-primaryText rounded-full p-0.5">
             <Input />
-            <Button variant="default" className="p-1"><Image src={arrowButton} alt='arrow-button' /></Button>
+            <Button variant="default" className="p-1">
+              <Image src={arrowButton} alt="arrow-button" />
+            </Button>
           </CardContent>
         </Card>
       </Section>
       <Section className="overflow-y-auto h-[calc(100dvh_-_32px)] w-[720px] flex flex-col gap-10 grow">
         <MainSectionBlock title={HomePage.mainTitle} titleSize="2xl">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint placeat soluta maiores natus doloribus porro. Eum, sequi neque velit pariatur unde quasi, iure laudantium, voluptatem rerum voluptates ipsum sint. Quam.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+            placeat soluta maiores natus doloribus porro. Eum, sequi neque velit
+            pariatur unde quasi, iure laudantium, voluptatem rerum voluptates
+            ipsum sint. Quam.
+          </p>
         </MainSectionBlock>
         <MainSectionBlock title="Skills" url="./">
           <div className="flex gap-4">
@@ -135,14 +172,14 @@ export default async function Home({
         </MainSectionBlock>
         <MainSectionBlock title="Recent Project" url="./">
           <div className="flex items-center justify-between gap-4">
-            {mockArticleData.map(infos => (
+            {mockArticleData.map((infos) => (
               <ArticleBlock key={infos.title} articleInfos={infos} />
             ))}
           </div>
         </MainSectionBlock>
         <MainSectionBlock title="Recent Article" url="./">
           <div className="flex items-center justify-between gap-4">
-            {mockArticleData.map(infos => (
+            {mockArticleData.map((infos) => (
               <ArticleBlock key={infos.title} articleInfos={infos} />
             ))}
           </div>
@@ -155,7 +192,11 @@ export default async function Home({
         </div>
         <Card className="p-0 pb-4">
           <CardHeader className=" rounded-t-lg overflow-hidden">
-            <Image src={me} alt='me' className=" h-40 object-cover mb-2 block"></Image>
+            <Image
+              src={me}
+              alt="me"
+              className=" h-40 object-cover mb-2 block"
+            ></Image>
           </CardHeader>
           <CardHeader className="px-4">
             <p>Information :</p>
@@ -170,32 +211,36 @@ export default async function Home({
           <CardHeader>Educational : </CardHeader>
           <CardContent>
             <p className="">
-              Department of Industrial Education,
-              National Taiwan Normal University
+              Department of Industrial Education, National Taiwan Normal
+              University
             </p>
-            <p className="text-secondaryText">
-              09 / 2017 - 06 / 2021
-            </p>
+            <p className="text-secondaryText">09 / 2017 - 06 / 2021</p>
           </CardContent>
         </Card>
         <Card>
           <CardHeader>Work Experience : </CardHeader>
           <CardContent>
             <Card className=" p-0 bg-white">
-              <Link className='flex gap-2 items-center' href=''>
+              <Link className="flex gap-2 items-center" href="">
                 <CardHeader className="overflow-hidden rounded-l-lg mb-0">
-                  <Image src={lionTravel} alt='lion-travel' className="w-20" />
+                  <Image src={lionTravel} alt="lion-travel" className="w-20" />
                 </CardHeader>
                 <CardContent>
                   <p className="text-black font-semibold">Lion Travel</p>
-                  <p className="text-secondaryText text-sm -mt-0.5">02 / 2022 ~ 02 / 2024</p>
+                  <p className="text-secondaryText text-sm -mt-0.5">
+                    02 / 2022 ~ 02 / 2024
+                  </p>
                 </CardContent>
               </Link>
             </Card>
           </CardContent>
         </Card>
         <Card className="p-0 overflow-hidden h-40 relative">
-          <Image src={bar} alt="bar" className="absolute -top-1/3 left-1/2 -translate-x-1/2"></Image>
+          <Image
+            src={bar}
+            alt="bar"
+            className="absolute -top-1/3 left-1/2 -translate-x-1/2"
+          ></Image>
         </Card>
       </Section>
     </main>
