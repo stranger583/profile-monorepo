b8d81c77b5a2993f730c1d5edceccc5f1a072311
stranger583 <zed042760@gmail.com>
2024-08-09 01:25:00 +0800

    feat: add react three fiber

--- apps/web/app/[lang]/about/page.tsx
+++ apps/web/app/[lang]/about/page.tsx
@@ -1,3 +1,23 @@
-export default function Page () {
-    return <div>About</div>
+'use client'
+import { Canvas } from "@react-three/fiber";
+import { Box } from "./box";
+
+export default function Page() {
+  return (
+    <div>
+      About
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </div>
+  );
 }
